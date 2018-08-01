f4f3b5084b693820212339ccb9117bdc4e6f6a42
beeceej <beeceej.code@gmail.com>
2018-08-01 15:14:54 -0500

    add back skip for incomplete test

--- pkg/interpreter/interpreter_test.go
+++ pkg/interpreter/interpreter_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestThing(t *testing.T) {
-	// t.Skip("More of an integration test for now")
+	t.Skip("More of an integration test for now")
 	i := Interpreter{}
 	str := `
 "func sayHi() { 
 	fmt.Println("Hello World, from iGo") 
-}"`
+}`
 
 	i.Interpret(str)
 	spew.Dump(i.Functions)
